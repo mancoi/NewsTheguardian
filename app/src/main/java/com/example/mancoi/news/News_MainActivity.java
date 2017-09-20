d8ddf72dac420f053fa377964942cf0e1b053648
mancoi <caoman96@gmail.com>
2017-09-20 13:46:22 +0700

    Change the side navigation bar to use NavigationView So no need to install the navigation list by hand anyomore, so remove the code

--- app/src/main/java/com/example/mancoi/news/News_MainActivity.java
+++ app/src/main/java/com/example/mancoi/news/News_MainActivity.java
@@ -6,23 +6,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class News_MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     TabLayout tabLayout;
-
-    private String[] mItems;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerListSection;
-    private ListView mDrawerListEdition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,24 +43,6 @@ public class News_MainActivity extends AppCompatActivity {
         //   3. Set the tab layout's tab names with the view pager's adapter's titles
         //      by calling onPageTitle()
         tabLayout.setupWithViewPager(viewPager);
-
-        //Set up the left sidebar
-        mItems = getResources().getStringArray(R.array.sidebar_item_labels);
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerListSection = (ListView) findViewById(R.id.left_drawer_section);
-        mDrawerListEdition = (ListView) findViewById(R.id.left_drawer_edition);
-
-        // Set the adapter for the list view
-        mDrawerListSection.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, mItems));
-
-        mItems = getResources().getStringArray(R.array.sidebar_edition_labels);
-        // Set the adapter for the list view
-        mDrawerListEdition.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItems));
-        // Set the list's click listener
-        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
 
     }
 
