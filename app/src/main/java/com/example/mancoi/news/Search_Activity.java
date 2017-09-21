ce8179d044eae85688eb60349da6f1851be10dfc
mancoi <caoman96@gmail.com>
2017-09-21 15:20:05 +0700

    Added Loader to Search_Activity

--- app/src/main/java/com/example/mancoi/news/Search_Activity.java
+++ app/src/main/java/com/example/mancoi/news/Search_Activity.java
@@ -5,20 +5,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mancoi on 07/09/2017.
  */
 
-public class Search_Activity extends AppCompatActivity {
+public class Search_Activity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>{
 
-    private final String HTTP_REQUEST = "https://content.guardianapis.com/";
+    private final String HTTP_REQUEST = "https://content.guardianapis.com/search?tag=environment/recycling&show-fields=headline,byline,thumbnail&api-key=test";
     private final String API_KEY = "3d076462-19d6-4cae-8d80-c3353eee520c";
+
+    private NewsAdapter mAdapter;
+
+    private int INTERNATIONAL_LOADER_ID = 1;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,6 +48,17 @@ public class Search_Activity extends AppCompatActivity {
             tv.setText(query);
         }
 
+        final ListView newsListItem = (ListView) findViewById(R.id.list_search_result);
+
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+        newsListItem.setAdapter(mAdapter);
+
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getSupportLoaderManager();
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(3, null, this);
     }
 
     @Override
@@ -71,6 +93,28 @@ public class Search_Activity extends AppCompatActivity {
             }
         });
         return true;
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this, HTTP_REQUEST);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+        //Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
     }
 
 }
