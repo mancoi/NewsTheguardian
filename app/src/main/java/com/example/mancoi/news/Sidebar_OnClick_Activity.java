03571019a55f52edac57e8ce7e7b907ad7fff165
mancoi <caoman96@gmail.com>
2017-10-16 02:24:12 +0700

    Add loading_indicator to sidebar_on_click

--- app/src/main/java/com/example/mancoi/news/Sidebar_OnClick_Activity.java
+++ app/src/main/java/com/example/mancoi/news/Sidebar_OnClick_Activity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -25,13 +27,13 @@ public class Sidebar_OnClick_Activity extends AppCompatActivity implements Loade
 
     private final String HTTP_REQUEST = "https://content.guardianapis.com/";
     private final String API_KEY = "3d076462-19d6-4cae-8d80-c3353eee520c";
-
+    LoaderManager loaderManager;
+    ProgressBar loaddingIndicator;
     private NewsAdapter mAdapter;
-
     private int LOADER_ID = 1;
-
     private String mIdToRetrieve;
     private String mGroupId;
+    private TextView emptyStateTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,13 +42,16 @@ public class Sidebar_OnClick_Activity extends AppCompatActivity implements Loade
 
         final ListView newsListItem = (ListView) findViewById(R.id.list);
 
-        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+        emptyStateTextView = (TextView) findViewById(R.id.empty_state_tv);
+        loaddingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
+
+        mAdapter = new NewsAdapter(Sidebar_OnClick_Activity.this, new ArrayList<News>());
         newsListItem.setAdapter(mAdapter);
+        newsListItem.setEmptyView(emptyStateTextView);
 
         Intent intent = getIntent();
         mIdToRetrieve = intent.getStringExtra("whatToRetrieve");
         mGroupId = intent.getStringExtra("groupID");
-        Toast.makeText(this, mIdToRetrieve, Toast.LENGTH_SHORT).show();
 
         //Set the toolbar to support actionBar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -56,11 +61,18 @@ public class Sidebar_OnClick_Activity extends AppCompatActivity implements Loade
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getSupportLoaderManager();
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(LOADER_ID, null, this);
+        loaderManager = getSupportLoaderManager();
+        if (QueryUtils.hasInternetConnection(Sidebar_OnClick_Activity.this)) {
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(LOADER_ID, null, this);
+
+        } else {
+            loaddingIndicator.setVisibility(View.GONE);
+            emptyStateTextView.setText(getResources().getString(R.string.no_internet));
+        }
 
         newsListItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -68,6 +80,20 @@ public class Sidebar_OnClick_Activity extends AppCompatActivity implements Loade
                 News newsExtra = mAdapter.getItem(position);
 
                 QueryUtils.StartIntent(Sidebar_OnClick_Activity.this, newsExtra);
+            }
+        });
+
+        emptyStateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loaddingIndicator.setVisibility(View.VISIBLE);
+                if (QueryUtils.hasInternetConnection(Sidebar_OnClick_Activity.this)) {
+                    loaderManager.restartLoader(LOADER_ID, null, Sidebar_OnClick_Activity.this);
+                } else {
+                    loaddingIndicator.setVisibility(View.GONE);
+                    Toast.makeText(Sidebar_OnClick_Activity.this, R.string.no_internet, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -94,13 +120,20 @@ public class Sidebar_OnClick_Activity extends AppCompatActivity implements Loade
 
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
-        //Clear the adapter of previous earthquake data
+        loaddingIndicator.setVisibility(View.GONE);
+        //Clear the adapter of previous news data
         mAdapter.clear();
 
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // If there is a valid list of {@link News}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (data != null && !data.isEmpty()) {
             mAdapter.addAll(data);
+        } else {
+            if (QueryUtils.hasInternetConnection(Sidebar_OnClick_Activity.this)) {
+                emptyStateTextView.setText(R.string.found_nothing);
+            } else {
+                emptyStateTextView.setText(R.string.no_internet);
+            }
         }
     }
 
